3bec7a62eee2b174ae58c7a1e000b45459a9dfa0
Hanieh24 <hanieh.ashouri@sptech.school>
2025-10-31 14:41:32 -0300

    atividade completo

--- src/main/java/school/sptech/Consultoria.java
+++ src/main/java/school/sptech/Consultoria.java
@@ -1,4 +1,118 @@
 package school.sptech;
 
+import school.sptech.especialistas.DesenvolvedorMobile;
+import school.sptech.especialistas.DesenvolvedorWeb;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Consultoria {
+    private String nome;
+    private Integer vagas;
+    private List<Desenvolvedor> desenvolvedores;
+
+    public void contratar(Desenvolvedor desenvolvedor){
+        if (vagas>desenvolvedores.size()){
+            desenvolvedores.add(desenvolvedor);
+        }
+    }
+
+    public void contratarFullstack(DesenvolvedorWeb desenvolvedor){
+        if (desenvolvedor.isFullstack()){
+            desenvolvedores.add(desenvolvedor);
+        }
+    }
+
+    public Double getTotalSalarios(){
+        Double soma = 0.0;
+        for (Desenvolvedor desenvolvedor : desenvolvedores){
+            soma += desenvolvedor.calcularSalario();
+        }
+        return soma;
+    }
+
+    public Integer qtdDesenvolvedoresMobile(){
+        Integer contagem = 0;
+        for (Desenvolvedor desenvolvedor : desenvolvedores){
+            if (desenvolvedor instanceof DesenvolvedorMobile){
+                contagem++;
+            }
+        }
+        return contagem;
+    }
+
+    public List<Desenvolvedor> buscarPorSalarioMaiorIgualQue(Double salario){
+        List<Desenvolvedor> resposta=new ArrayList<>();
+        for (Desenvolvedor desenvolvedor : desenvolvedores){
+            if (desenvolvedor.calcularSalario() >= salario){
+                resposta.add(desenvolvedor);
+            }
+        }
+        return resposta;
+    }
+
+    public Desenvolvedor buscarMenorSalario(){
+        if (desenvolvedores.isEmpty()){
+            return null;
+        }else {
+            Double menor =desenvolvedores.get(0).calcularSalario();
+            Desenvolvedor desenvolvedorMenor = desenvolvedores.get(0);
+            for (Desenvolvedor desenvolvedor : desenvolvedores){
+                if (desenvolvedor.calcularSalario() < menor){
+                    menor = desenvolvedor.calcularSalario();
+                    desenvolvedorMenor = desenvolvedor;
+                }
+            }
+            return desenvolvedorMenor;
+        }
+    }
+
+    public List<Desenvolvedor> buscarPorTecnologia(String tecnologia){
+        List<Desenvolvedor> resposta = new ArrayList<>();
+
+        for (Desenvolvedor dev : desenvolvedores){
+            if (dev instanceof DesenvolvedorMobile){
+
+                if (((DesenvolvedorMobile) dev).getPlataforma().equalsIgnoreCase(tecnologia) ||
+                                ((DesenvolvedorMobile) dev).getLinguagem().equalsIgnoreCase(tecnologia)){
+                    resposta.add(dev);
+                }
+            }else if(dev instanceof DesenvolvedorWeb){
+                if (((DesenvolvedorWeb) dev).getFrontend().equalsIgnoreCase(tecnologia)||
+                                ((DesenvolvedorWeb) dev).getBackend().equalsIgnoreCase(tecnologia)||
+                                ((DesenvolvedorWeb) dev).getSgbd().equalsIgnoreCase(tecnologia)){
+                    resposta.add(dev);
+                }
+            }
+        }
+        return resposta;
+    }
+
+    public Double getTotalSalariosPorTecnologia(String tecnologia){
+        Double soma = 0.0;
+        List <Desenvolvedor> novo = buscarPorTecnologia(tecnologia);
+
+        for (Desenvolvedor desenvolvedor : novo){
+            soma +=desenvolvedor.calcularSalario();
+        }
+
+        return soma;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getVagas() {
+        return vagas;
+    }
+
+    public void setVagas(Integer vagas) {
+        this.vagas = vagas;
+    }
 }
--- src/main/java/school/sptech/especialistas/DesenvolvedorWeb.java
+++ src/main/java/school/sptech/especialistas/DesenvolvedorWeb.java
@@ -1,4 +1,52 @@
 package school.sptech.especialistas;
 
-public class DesenvolvedorWeb {
+import school.sptech.Desenvolvedor;
+
+import javax.swing.*;
+
+public class DesenvolvedorWeb extends Desenvolvedor {
+    private String backend;
+    private String frontend;
+    private String sgbd;
+    private Integer horasMentoria;
+
+    public Double calcularSalario(){
+        return (getValorHora()*getQtdHoras())+(horasMentoria*300);
+    }
+
+    public Boolean isFullstack(){
+        return frontend != null && backend != null && sgbd != null && horasMentoria != null;
+    }
+
+    public String getBackend() {
+        return backend;
+    }
+
+    public void setBackend(String backend) {
+        this.backend = backend;
+    }
+
+    public String getFrontend() {
+        return frontend;
+    }
+
+    public void setFrontend(String frontend) {
+        this.frontend = frontend;
+    }
+
+    public String getSgbd() {
+        return sgbd;
+    }
+
+    public void setSgbd(String sgbd) {
+        this.sgbd = sgbd;
+    }
+
+    public void setHorasMentoria(Integer horasMentoria) {
+        this.horasMentoria = horasMentoria;
+    }
+
+    public Integer getHorasMentoria() {
+        return horasMentoria;
+    }
 }
